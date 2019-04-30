3399ce4363b8e11ff80c8b41d0193a8f67d89365
stevenpfiggins <sfiggins43@gmail.com>
2019-04-30 10:49:28 -0400

    Add project files.

---
+++ ToolRenter.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToolRenter.MVC.Startup))]
+namespace ToolRenter.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
