d3cb4e20baf9482ac0a0902165b34c7fc7f9ab10
George Go <gngo2018@gmail.com>
2019-04-30 12:40:38 -0400

    George Branch Create

---
+++ ToolRenter.Models/Equipment/EquipmentCreate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolRenter.Models.Equipment
+{
+    public class EquipmentCreate
+    {
+    }
+}
---
+++ ToolRenter.Services/EquipmentService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolRenter.Services
+{
+    public class EquipmentService
+    {
+    }
+}
