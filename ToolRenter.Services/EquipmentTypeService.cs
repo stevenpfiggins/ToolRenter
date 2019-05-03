f1baa7d9a07a5ff2e7747ffd4034715f88a8844d
George Go <gngo2018@gmail.com>
2019-05-03 10:16:44 -0400

    Models Service Controller Made

---
+++ ToolRenter.Services/EquipmentTypeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolRenter.Services
+{
+    public class EquipmentTypeService
+    {
+        private readonly Guid _userId;
+
+        public EquipmentTypeService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //Get All Equipment Types
+
+        //Get Equipment Type by Id
+
+        //Create Equipment Type
+
+        //Edit Equipment Type
+        
+        //Delete Equipment Type
+    }
+}
---
+++ ToolRenter.WebAPI/Controllers/EquipmentTypeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToolRenter.Services;
+
+namespace ToolRenter.WebAPI.Controllers
+{
+    public class EquipmentTypeController : ApiController
+    {
+        //GET All Equipment Types
+
+        private EquipmentTypeService CreateEquipmentTypeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var svc = new EquipmentTypeService(userId);
+
+            return svc;
+        }
+    }
+}
