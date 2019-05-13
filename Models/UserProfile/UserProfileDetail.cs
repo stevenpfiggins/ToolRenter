b7bc0cb98f5213ef2ad127ba4009b143d1c81f7d
stevenpfiggins <sfiggins43@gmail.com>
2019-05-13 11:42:45 -0400

    Added User Profile entity, models, service, and controller

---
+++ Models/UserProfile/UserProfileDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolRenter.Models.UserProfile
+{
+    public class UserProfileDetail
+    {
+        public int UserProfileId { get; set; }
+
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+    }
+}
